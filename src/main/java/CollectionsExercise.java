b4c138f8581fb7b2c797ee6af1488fba500ab02a
rutugs <rutujashinde1696@gmail.com>
2021-12-08 19:26:32 +0530

    commit for data-strutures CollectionsExercise

---
+++ src/main/java/CollectionsExercise.java
@@ -0,0 +1,32 @@
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Set;
+
+public class CollectionsExercise {
+
+  void addToEndOfLinkedList(LinkedList<Integer> numbers, int i) {
+
+    numbers.add(i);
+
+  }
+
+  void addToStartOfLinkedList(LinkedList<Integer> numbers, int i) {
+    numbers.add(0, i);
+  }
+
+  void removeItemFromTopOfStack(Deque<Integer> stack) {
+    //stack.pop();
+    stack.poll();
+  }
+
+  void removeItemFromFrontOfQueue(Queue<Integer> queue) {
+      //queue.poll();
+        queue.remove();
+  }
+
+  void addItemToTreeSet(Set<String> set, String item) {
+        set.add(item);
+  }
+
+}
